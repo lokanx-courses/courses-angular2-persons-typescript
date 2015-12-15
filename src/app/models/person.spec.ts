296fc65abc44441dd822d3584e0e6a8dd0e635fc
lokanx <lokanx@users.noreply.github.com>
2015-12-15 11:54:31 +0100

    Added base for unit testing

---
+++ src/app/models/person.spec.ts
@@ -0,0 +1,9 @@
+import {Person} from './person';
+
+describe('Person', function() {
+    it('has name given in the constructor', function() {
+      var person = new Person(1, 'Test Testingsson', 'test.testingsson@testability.com', 'Tester Town');
+      expect(person.name).toEqual('Test Testingsson');
+    });
+
+});
